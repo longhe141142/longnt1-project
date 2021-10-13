07dda2f60f29e719f537ec5ecab588bd24ee5dd8
long he141142 <longnt1@vmodev.com>
2021-10-13 17:31:34 +0700

    api unit test

--- _src/test/index.js
+++ _src/test/index.js
@@ -1,80 +1,10 @@
 const {
   InputSelection,
-  GetUserText,
-  UserMenu,
+
   mainMenu,
 } = require("./helper/helper");
-let {
-  UpdateProfiletest,
-  viewProfileTest,
-  viewEmployeeList,
-  viewOwnEmployee,
-  addEmployeeTest,
-} = require("./user");
 
-let userApi = () => {
-  let option = InputSelection(
-    "Enter input(0 to exit)->: ",
-    "Invalid Value",
-    UserMenu(),
-    0,
-    5
-  );
-  if (option === 0) {
-    return;
-  }
-  switch (option) {
-    case 1:
-      UpdateProfiletest();
-      break;
-    case 2:
-      viewEmployeeList();
-      break;
-    case 3:
-      viewOwnEmployee();
-      break;
-    case 4:
-      addEmployeeTest();
-      break;
-    case 5:
-      viewProfileTest();
-    default:
-      break;
-  }
-  return;
-};
-
-let formApi = () => {
-  let option = InputSelection(
-    "Enter input(0 to exit)->: ",
-    "Invalid Value",
-    UserMenu(),
-    0,
-    5
-  );
-  if (option === 0) {
-    return;
-  }
-  switch (option) {
-    case 1:
-      UpdateProfiletest();
-      break;
-    case 2:
-      viewEmployeeList();
-      break;
-    case 3:
-      viewOwnEmployee();
-      break;
-    case 4:
-      addEmployeeTest();
-      break;
-    case 5:
-      viewProfileTest();
-    default:
-      break;
-  }
-  return;
-};
+const { userApi } = require("./helper/functionInterface");
 
 let main = () => {
   //  let option =
